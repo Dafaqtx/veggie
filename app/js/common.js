b3cfc6749050317bdb608cfbb750b0c6139a9596
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-16 18:43:31 +0600

    add slow scroll into block

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
-$(function() {
-
-	// Custom JS
-
+$(document).ready(function () {
+	$('a[href^="#"], a[href^="."]').click(function () {
+		var scroll_el = $(this).attr('href');
+		if ($(scroll_el).length != 0) {
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700);
+		}
+		return false;
+	});
 });
