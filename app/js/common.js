8fda9b4caa4b70d7dd4fbc54296f7fff8ae37658
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-17 12:17:12 +0600

    add menu block

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
 	$('a[href^="#"], a[href^="."]').click(function () {
 		var scroll_el = $(this).attr('href');
 		if ($(scroll_el).length != 0) {
@@ -6,4 +5,15 @@ $(document).ready(function () {
 		}
 		return false;
 	});
-});
+
+	(function ($) {
+		$(function () {
+
+			$('ul.menu__list').on('click', 'li:not(.active)', function () {
+				$(this)
+					.addClass('active').siblings().removeClass('active')
+					.closest('.menu__tabs').find('div.menu__content').removeClass('active').eq($(this).index()).addClass('active');
+			});
+
+		});
+	})(jQuery);
