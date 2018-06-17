b32eb19cee098868b6b106c2a2f8400ccf43fe9e
Alexander Kuchuk <dafaqtx@gmail.com>
2018-06-17 15:24:46 +0600

    add responsive styles

--- app/js/common.js
+++ app/js/common.js
@@ -1,32 +1,35 @@
-	$('a[href^="#"], a[href^="."]').click(function () {
-		var scroll_el = $(this).attr('href');
-		if ($(scroll_el).length != 0) {
-			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700);
-		}
-		return false;
-	});
+$('a[href^="#"], a[href^="."]').click(function () {
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length != 0) {
+		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700);
+	}
+	return false;
+});
 
-	(function ($) {
-		$(function () {
+(function ($) {
+	$(function () {
 
-			$('ul.menu__list').on('click', 'li:not(.active)', function () {
-				$(this)
-					.addClass('active').siblings().removeClass('active')
-					.closest('.menu__tabs').find('div.menu__content').removeClass('active').eq($(this).index()).addClass('active');
-			});
-
+		$('ul.menu__list').on('click', 'li:not(.active)', function () {
+			$(this)
+				.addClass('active').siblings().removeClass('active')
+				.closest('.menu__tabs').find('div.menu__content').removeClass('active').eq($(this).index()).addClass('active');
 		});
-	})(jQuery);
-	
-	$(window).scroll(function () {
-		
-		if ($(this).scrollTop() >= 700) {
-			$('a.up').css('opacity', '1');
-		} else {
-			$('a.up').css('opacity', '0')
-		}
 
-	})
+	});
+})(jQuery);
 	
+$(window).scroll(function () {
+	if ($(this).scrollTop() >= 700) {
+		$('a.up').css('opacity', '1');
+	} else {
+		$('a.up').css('opacity', '0')
+	}
+})
+
+$('.header__burger').on('click', function() {
+	$(this).toggleClass('header__burger--active');
+
+	$('.header__menu').toggleClass('header__menu--open');
+})
 	
 	
